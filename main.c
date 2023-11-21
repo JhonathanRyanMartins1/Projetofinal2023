9aa1f64903354a1eeae33fb8c19a20f825f4657c
JhonathanRyanMartins1 <151565493+JhonathanRyanMartins1@users.noreply.github.com>
2023-11-21 15:26:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,13 +3,32 @@
 #include <math.h>
 int main(){
     typedef struct {
-        int dia 
-        int mes
-        int ano
+        int dia;
+        int mes;
+        int ano;
     } Data;
     
     
+    
+    typedef struct{
+       char nome_do_cliente[50];
+       int telefone_do_cliente[15];
+    }Cliente;
+    
+    
+    
+    typedef struct {
+        char cliente;
+        char nome_do_animal[50];
+        int espécie;
+        Data data_nascimento;
+        char agressivo;
+    }Animal;
+    
+    
+    
+    typedef struct {
         
+    }   
     return 0;
 }
-
