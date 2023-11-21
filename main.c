d49077cb8f404ec01014f17885b60e81216af43a
JhonathanRyanMartins1 <151565493+JhonathanRyanMartins1@users.noreply.github.com>
2023-11-21 14:52:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+int main(){
+    typedef struct {
+        int dia 
+        int mes
+        int ano
+    } Data;
+    
+    
+        
+    return 0;
+}
 
