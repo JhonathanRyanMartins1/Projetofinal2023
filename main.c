1f09bee25a5c40c8ea7d81ef75b8469b96212c7a
JhonathanRyanMartins1 <151565493+JhonathanRyanMartins1@users.noreply.github.com>
2023-11-23 23:21:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ typedef struct {
     char nome_do_animal[50];
     char especie; 
     Data data_nascimento;
-    int agressivo; 
+    char agressivo; 
 } Animal;
 
 typedef struct {
@@ -40,45 +40,64 @@ int main() {
 
     printf("Qual o dia desejado para a consulta?\n");
     scanf("%d", &meuServico.data_servico.dia);
-
+     if (meuServico.data_servico.dia < 1 || meuServico.data_servico.dia > 31) {
+        printf("Dia inválido. O dia deve estar entre 1 e 31.\n");
+        return 1; 
+    }
     printf("Qual o mes desejado para a consulta?\n");
     scanf("%d", &meuServico.data_servico.mes);
-
+if (meuServico.data_servico.mes < 1 || meuServico.data_servico.mes > 12) {
+        printf("Mês inválido. O mês deve estar entre 1 e 12.\n");
+        return 1;
+}
     printf("Qual o ano desejado para a consulta?\n");
     scanf("%d", &meuServico.data_servico.ano);
     printf("A data desejada para a consulta é: %02d/%02d/%04d\n\n\n", meuServico.data_servico.dia, meuServico.data_servico.mes, meuServico.data_servico.ano);
-
+if (meuServico.data_servico.ano < 2000 || meuServico.data_servico.ano > 2023) {
+        printf("Ano inválido. O ano deve estar entre 2000 e 2023.\n");
+        return 1;
+}
     printf("Qual o seu nome completo?\n");
-    scanf("%s", meuServico.cliente.nome_do_cliente);
+    scanf("%49s", meuServico.cliente.nome_do_cliente);
     
     printf("Qual seu numero de telefone:\n");
-    scanf("%s", meuServico.cliente.telefone_do_cliente);
+    scanf("%14s", meuServico.cliente.telefone_do_cliente);
     
     printf("Senhor(a): %s, poderia informar o nome do Animal?\n", meuServico.cliente.nome_do_cliente);
     scanf("%s", meuServico.pet.nome_do_animal);
     printf("Digite o número que corresponde a especie do seu animal:");
-    printf("\n1.Cachorro\n2.Gato\n3.Passaro\n4.Outro\n");
+    printf("Digite o número que corresponde à espécie do seu animal:\n");
+    printf("1. Cachorro\n2. Gato\n3. Pássaro\n4. Outro\n");
     scanf("%d", &numero);
-   switch (numero) {
-        case 1:
-            printf("Especie selecionada: Cachorro\n");
-            break;
-        case 2:
-            printf("Especie selecionada: Gato\n");
-            
-            break;
-        case 3:
-            printf("Especie selecionada: Passaro\n");
-            
-            break;
-        case 4:
-            printf("Especie selecionada: Outro\n");
-            
-            break;
-        default:
-            printf("Opção inválida\n");
-            return 1;
+
+switch (numero) {
+    case 1:
+        printf("Espécie selecionada: Cachorro\n");
+        meuServico.pet.especie = 'C';
+        break;
+    case 2:
+        printf("Espécie selecionada: Gato\n");
+        meuServico.pet.especie = 'G';
+        break;
+    case 3:
+        printf("Espécie selecionada: Pássaro\n");
+        meuServico.pet.especie = 'P';
+        break;
+    case 4:
+        printf("Espécie selecionada: Outro\n");
+        meuServico.pet.especie = 'O';
+        break;
+    default:
+        printf("Opção inválida\n");
+        return 1;
 }
+
+    printf("Por favor, informe a data de nascimento do animal (dia mês ano):\n");
+    scanf("%d %d %d", &meuServico.pet.data_nascimento.dia, &meuServico.pet.data_nascimento.mes, &meuServico.pet.data_nascimento.ano);
+    
+    printf("O seu animal é agressivo?\n");
+    printf("Digite (S) para sim e (N) para não\n ");
+    scanf(" %c", &meuServico.pet.agressivo);  
 
     
             return 0;
