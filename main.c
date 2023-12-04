8a80013b907d1ad27c1135ed06fae2e39eaff7d7
JhonathanRyanMartins1 <151565493+JhonathanRyanMartins1@users.noreply.github.com>
2023-12-04 15:02:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ Trabalho Final Algoritmos 1
 #define MAX_DIA 31
 #define MIN_MES 1
 #define MAX_MES 12
-#define MIN_ANO 2000
+#define MIN_ANO 2023
 #define MAX_ANO 2050
 
 typedef struct {
@@ -54,19 +54,72 @@ int main() {
         raçapassaro;
     meuServico.identificador = 1;
     meuServico.pago = 'N'; 
-
+int cadastro;
+char animal_cadastrado;
 printf("***************************************\n");
     printf("* Bem-vindo(a) ao Sistema Veterinário *\n");
     printf("***************************************\n\n");
     
-    
+    printf("O Senhor(a) ja tem algum animal cadrastado?\n");
+    printf("Digite (1) para sim e (2) para não\n");
+    scanf("%d", &cadastro);
+    switch (cadastro){
+        case 1:
+        printf("Otimo!!!Prossiga\n");
+        break;
+        case 2:
+        printf("Prossiga para o Sistema de cadastro:\n");
+        printf("Qual o seu nome?\n");
+        scanf("%s", meuServico.cliente.nome_do_cliente);
+        do{
+        printf("Qual seu número de telefone:\n");
+        scanf("%14s", meuServico.cliente.telefone_do_cliente);
+        if(strlen(meuServico.cliente.telefone_do_cliente) != 12){
+            printf("Por favor, informe um número valido (DDD + 9 adicional + 8 digitos).\n");
+        }
+        }while(strlen(meuServico.cliente.telefone_do_cliente) != 12);
+        printf("Senhor(a): %s, poderia informar o nome do animal?\n", meuServico.cliente.nome_do_cliente);
+        scanf("%s", meuServico.pet.nome_do_animal);
+        
+        printf("Digite o número que corresponde à espécie do seu animal:\n");
+        printf("1. Cachorro\n2. Gato\n3. Pássaro\n4. Outro\n");
+        scanf("%d", &numero);
+        if(numero = 0){
+            printf("Espécie selecionada: Cachorro\n");
+                printf("Digite a raça do %s :\n", meuServico.pet.nome_do_animal);
+                scanf("%d", &raçacachorro);
+        }
+        if( numero = 1){
+            printf("Espécie selecionada: Gato\n"); 
+            printf("Digite a raça do %s :\n", meuServico.pet.nome_do_animal);
+            scanf("%d", &raçagato);
+        }
+        if (numero = 2){
+            printf("Espécie selecionada: Pássaro\n");
+            printf("Digite a raça do %s :\n", meuServico.pet.nome_do_animal);
+            scanf("%d", &raçapassaro);
+        }
+        if (numero = 3){
+            printf("Espécie selecionada: Pássaro\n");
+            printf("Digite a raça do %s :\n", meuServico.pet.nome_do_animal);
+            scanf("%d", &raçapassaro);
+        }
+        if (numero = 4 ){
+            printf("Espécie selecionada: Outro\n");
+        }
+        
+        
+        
+        printf("Animal cadastrado!!!");
+        break;
+    } 
     printf("Qual o dia desejado para a consulta?\n");
     scanf("%d", &meuServico.data_servico.dia);
     while (meuServico.data_servico.dia < MIN_DIA || meuServico.data_servico.dia > MAX_DIA) {
         printf("Dia inválido. O dia deve estar entre 1 e 31.\n");
         printf("Qual o dia desejado para a consulta?\n");
         scanf("%d", &meuServico.data_servico.dia);
-    }
+    
 
     printf("Qual o mês desejado para a consulta?\n");
     scanf("%d", &meuServico.data_servico.mes);
@@ -85,7 +138,7 @@ printf("***************************************\n");
     scanf("%d", &meuServico.data_servico.ano);
 
     if (meuServico.data_servico.ano < MIN_ANO || meuServico.data_servico.ano > MAX_ANO) {
-        printf("Ano inválido. O ano deve estar entre 2000 e 2050.\n");
+        printf("Ano inválido. O ano deve estar entre 2000 e 2023.\n");
     while (meuServico.data_servico.ano < MIN_ANO || meuServico.data_servico.ano > MAX_ANO) {
         printf("Qual o ano desejado para a consulta?\n");
         scanf("%d", &meuServico.data_servico.ano);
@@ -98,7 +151,7 @@ printf("***************************************\n");
     printf("Qual o seu nome?\n");
     scanf("%49s", meuServico.cliente.nome_do_cliente);
     
-    do{
+  do{
         printf("Qual seu número de telefone:\n");
         scanf("%14s", meuServico.cliente.telefone_do_cliente);
         if(strlen(meuServico.cliente.telefone_do_cliente) != 12){
@@ -113,11 +166,12 @@ printf("***************************************\n");
         printf("Digite o número que corresponde à espécie do seu animal:\n");
         printf("1. Cachorro\n2. Gato\n3. Pássaro\n4. Outro\n");
         scanf("%d", &numero);
-
+    
+        printf("Espécie selecionada: Cachorro\n");
+                printf(for (;;) {
         switch (numero) {
             case 1:
-                printf("Espécie selecionada: Cachorro\n");
-                printf("Digite a raça do %s :\n", meuServico.pet.nome_do_animal);
+            "Digite a raça do %s :\n", meuServico.pet.nome_do_animal);
                 scanf("%d", &raçacachorro);
                 meuServico.pet.especie = 'C';
                 break;
@@ -140,7 +194,9 @@ printf("***************************************\n");
             default:
                 printf("Opção inválida\n");
                 continue;
+
         }
+}
 
         break;
     }
@@ -203,4 +259,6 @@ void verificarPagamento(char pago) {
     } else {
         printf("Opção inválida.\n");
     }
+    printf("Obrigado pela preferencia");
+}
 }
